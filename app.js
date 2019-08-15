b42b7cf27c91ada45309dac84e07f7a0c5111e87
vue <aixiafeifly@163.com>
2019-08-15 16:15:52 +0800

    注册和页面

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ App({
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
-  onLaunch: function () {
-    console.log('onLaunch')
+  onLaunch: function (options) {
+    console.log(options,)
+    //异步会比onshow后执行
+    
   },
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
-    console.log(options)
+    console.log(options.scene,'show')
+    switch (options.scene){
+      case 1006:
+      console.log('1006执行')
+      break;
+    }
+
+
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res)
+      }
+    })
   },
 
   /**
@@ -20,12 +34,19 @@ App({
   onHide: function () {
     console.log('hide')
   },
-
+  
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
   onError: function (msg) {
     console.log(msg,'msg')
+   
+  },
+  globalData:{
+    name:"alax",
+    age:18
   }
+
+  
 })
             
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,41 +1,83 @@
+const appGlobel=getApp()
+console.log(appGlobel.globalData) 
+
+
 Page({
+
   /**
    * 页面的初始数据
    */
   data: {
-    msg:"这是首页",
-    counter:0,
-    list:[
-      { name: 'JOIN1', age: "12", max: "女" },
-      { name: 'JOIN2', age: "12", max: "女" },
-      { name: 'JOIN3', age: "12", max: "女" },
-      { name: 'JOIN4', age: "12", max: "女" }
-    ]
+    list:[]
   },
-  handAdd:function(){
-  
-   var coun= this.data.counter
-    coun++
-    if (coun<0){
-      coun=0
-    }
-    this.setData(
-      {
-        counter: coun
-      }
-    )
+  handleGetUserInfo(event){
+    console.log(event,'拿到授权信息')
+    console.log(event.detail)
+   
   },
-  handReduce:function(){
-    var coun = this.data.counter
-    coun--
-    if (coun < 0) {
-      coun = 0
-    }
-    this.setData(
-      {
-        counter: coun
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    console.log(options,'这里是拿到链接后面得参数')
+     const that = this
+    wx.request({
+      url: 'http://123.207.32.32:8000//api/v1/recommend', //仅为示例，并非真实的接口地址
+      success:function(res){   //这里必须是箭头函数 否则这里得this是找不到的，
+        const data = res.data.data.list
+        that.setData({
+          list:data
+        })
       }
-    )
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    console.log("ready")
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    console.log('show')
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide () {
+    console.log('hide')
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+    console.log('页面跳转')
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+    console.log('下来刷新')
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+    console.log
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+    console.log('分享')
   }
- 
 })
